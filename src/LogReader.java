7573877f4a0622694f1b3da06f4ae1d19c05735b
Karpaga Ganesh Patchirajan <karpagaganesh_patchirajan@intuit.com>
2017-04-08 20:32:04 -0500

    MO Domain

--- src/LogReader.java
+++ src/LogReader.java
@@ -4,15 +4,29 @@ import java.io.InputStreamReader;
 
 public class LogReader {
 
+    private final String lineSplitter = "=================================================================================================================";
     private void readLogFile(String logFileName)
     {
         try{
             FileInputStream fstream = new FileInputStream(logFileName);
             BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
             String strLine;
+            boolean newModel = false;
+            int lineSplitterCount = 0;
             while ((strLine = br.readLine()) != null)   {
-
+                String newLine = strLine.trim();
+                if(lineSplitter.equalsIgnoreCase(newLine)){
+                    lineSplitterCount++;
+                }
+                if(strLine.contains("MO   ")){
+                    System.out.println();
+                    String name = getMOName(newLine);
+                }
                 System.out.println (strLine);
+                if(lineSplitterCount==2){
+                    lineSplitterCount = 0;
+                    newModel = true;
+                }
             }
 
         } catch (Exception e) {
@@ -20,6 +34,20 @@ public class LogReader {
         }
 
     }
+
+    private String getMOName(String newLine) {
+        String moName = "";
+        String[] split = newLine.split(" ");
+        if(split[0].equalsIgnoreCase("MO")){
+            int i = 1;
+            while (split[i].equalsIgnoreCase("")){
+                i++;
+            }
+            moName = split[i];
+        }
+        return moName;
+    }
+
     public static void main(String[] args) {
         LogReader logReader = new LogReader();
         logReader.readLogFile("030619_DUTTON_ROAD.log");
